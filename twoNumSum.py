359c140264f3b98bad6ea4ecf186767a18ef29ce
Craig Opie <craig@deathstardev.com>
2022-07-06 10:54:48 -1000

    added a few questions and solutions

---
+++ twoNumSum.py
@@ -0,0 +1,31 @@
+"""
+    Write a function -> takes a non-empty array of integers and an integer of target sum.
+    If any two numbers match the target, then return them in an array (any order).
+    Else, return an empty array.
+
+    Example Return: [1, 11]
+"""
+def twoNumberSum(array, targetSum):
+    """ 
+        Description: Function takes a list of integers and finds summing pairs and returns
+        the pairs in a list.
+    
+        Parameters:
+            array - list of integers
+            targetSum - integer
+
+        Returns:
+            sums - list of pairs 
+    """
+    # store the array in a set to speed up searching
+    temp = set(num for num in array)
+
+    # iterate through each number in the array and look for the summing pair in temp
+    # return the pair if it exists
+    for num in array:
+        target = targetSum - num
+        if target in temp and target is not num:
+            return [num, target]
+
+    # return an empty list if a pair does not exist
+    return []
---
+++ validateSubsequence.py
@@ -0,0 +1,36 @@
+"""
+    Given two non-empty arrays of integers, write a function that determines
+    whether the second array is a subsequence of the first one.
+
+    A subsequence of an array is a set of numbers that aren't necessarily adjacent
+    in the array but that are in the same order as they appear in the array. For
+    instance, the numbers [1, 3, 4] form a subsequence in the array [1, 2, 3, 4], 
+    and so do the numbers [2, 4].  Note that a single number in an array and the
+    array itself are both valid subsequences of the array.
+"""
+def isValidSubsequence(array, sequence):
+    """
+        Description:  Function takes an array and a sequence of numbers and searches
+        the array to see if the sequence appears as a subsequence.
+
+        Parameters:
+            array - list of integers
+            sequence - list of integers
+
+        Returns:
+            true or false
+    """
+    # create iteration index tracker
+    index_ = 0
+
+    # iterate throught the array and search for each integer in the sequence using
+    # an index to track the sequence value being searched.  returns true if the
+    # search iterates through all of the sequence.
+    for num in array:
+        if index_ == len(sequence):
+            return True
+        if num == sequence[index_]:
+            index_ += 1
+
+    # returns false if the sequence was not fully iterated through
+    return index_ == len(sequence)
